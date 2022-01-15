a711ba7440521cea58b1d6eba232730a4e00fe9a
Bay1ts <Bay1ts@gmail.com>
2022-01-15 11:17:27 +0800

    test

--- SiriusGeo.go
+++ SiriusGeo.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"github.com/bluele/gcache"
 	"github.com/ip2location/ip2location-go/v9"
 	"io/ioutil"
 	"log"
@@ -18,7 +19,7 @@ var httpClient = &http.Client{
 	Timeout: time.Second * 2,
 }
 
-var cache = make(map[string]struct{})
+//var cache = make(map[string]struct{})
 
 type Config struct {
 	DatabaseFilePath     string   // Path to ip2location database file
@@ -40,10 +41,10 @@ func CreateConfig() *Config {
 }
 
 type Plugin struct {
-	next http.Handler
-	name string
-	db   *ip2location.DB
-
+	next                 http.Handler
+	name                 string
+	db                   *ip2location.DB
+	lruCache             gcache.Cache
 	modSecurityUrl       string
 	allowedCountries     []string
 	allowedIps           []string
@@ -68,6 +69,7 @@ func New(ctx context.Context, next http.Handler, cfg *Config, name string) (http
 		return nil, fmt.Errorf("%s: failed to open database: %w", name, err)
 	}
 
+	lruCache := gcache.New(1024).LRU().Build()
 	return &Plugin{
 		db:                   db,
 		next:                 next,
@@ -78,6 +80,7 @@ func New(ctx context.Context, next http.Handler, cfg *Config, name string) (http
 		disallowedStatusCode: cfg.DisallowedStatusCode,
 		modSecurityUrl:       cfg.ModSecurityUrl,
 		privateIPRanges:      InitPrivateIPBlocks(),
+		lruCache:             lruCache,
 	}, nil
 }
 func (p Plugin) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
@@ -107,7 +110,8 @@ func (p Plugin) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 		proxyReq.Header[h] = val
 	}
 	for _, ip := range p.GetRemoteIPs(req) {
-		if _, v := cache[ip]; v || !p.CheckAllowed(ip) {
+		//if _, v := cache[ip]; v || !p.CheckAllowed(ip) {
+		if p.lruCache.Has(ip) || !p.CheckAllowed(ip) {
 			log.Printf("%s: %v", p.name, "禁止访问")
 			rw.WriteHeader(p.disallowedStatusCode)
 			rw.Header().Set("Content-Type", "text/html; charset=utf-8")
@@ -129,7 +133,8 @@ func (p Plugin) CallWaf(ip string, rw http.ResponseWriter, proxyReq *http.Reques
 	if resp.StatusCode >= 400 {
 		//block
 		log.Printf("%s: %v触发防火墙", p.name, ip)
-		cache[ip] = struct{}{}
+		//cache[ip] = struct{}{}
+		p.lruCache.SetWithExpire(ip, nil, time.Minute*30)
 		return
 	}
 }
